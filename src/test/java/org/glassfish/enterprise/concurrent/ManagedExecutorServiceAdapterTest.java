87d78405231395ca90853d136850fb347838dea4
Petr Aubrecht <aubrecht@asoftware.cz>
2024-02-15 11:12:27 +0100

    update copyright years

--- src/test/java/org/glassfish/enterprise/concurrent/ManagedExecutorServiceAdapterTest.java
+++ src/test/java/org/glassfish/enterprise/concurrent/ManagedExecutorServiceAdapterTest.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2010, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024 Payara Foundation and/or its affiliates.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -12,11 +13,6 @@
  * https://www.gnu.org/software/classpath/license.html.
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
- */
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
  */
 package org.glassfish.enterprise.concurrent;
 
